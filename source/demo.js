2634a526b098ee6189b6a72057df65fbf2b9d497
nathansmith <nathan@sonspring.com>
2018-09-06 20:21:20 -0500

    Adding a "demo" mode, that shows via `npm start`.

---
+++ source/demo.js
@@ -0,0 +1,35 @@
+// Dependencies.
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// CSS.
+import './index.css'
+
+// UI components.
+import Render from './'
+
+// Define class.
+class Demo extends React.Component {
+  // Render method.
+  render () {
+    // Boolean.
+    const bool = true
+
+    // Expose UI.
+    return (
+      <React.Fragment>
+        <Render if={bool}>
+          <p>
+            If you are reading this the component works.
+          </p>
+        </Render>
+      </React.Fragment>
+    )
+  }
+}
+
+// Get root.
+const root = document.getElementById('root')
+
+// Render.
+ReactDOM.render(<Demo />, root)
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,86 @@
+// =============
 // Dependencies.
+// =============
+
+const HtmlWebPackPlugin =
+  require('html-webpack-plugin')
+
 const path = require('path')
 
+// ========
 // Loaders.
+// ========
+
 const rules = [
   {
     test: /\.js$/,
     use: 'babel-loader'
+  },
+  {
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'postcss-loader'
+    ]
   }
 ]
 
-// Export.
-module.exports = {
-  mode: 'production',
+// ========
+// Plugins.
+// ========
+
+const plugins = [
+  new HtmlWebPackPlugin({
+    template: './source/demo.html',
+    filename: './index.html'
+  })
+]
+
+// ====================
+// Config: Development.
+// ====================
+
+const configDevelopment = {
+  plugins,
   module: {
     rules
   },
-  entry: './source',
+  entry: './source/demo.js',
+  output: {
+    filename: 'bundle.[hash].js'
+  }
+}
+
+// ===================
+// Config: Production.
+// ===================
+
+const configProduction = {
+  plugins,
+  module: {
+    rules
+  },
+  entry: './source/index.js',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
   }
+}
+
+// =======
+// Export.
+// =======
+
+module.exports = (env, args) => {
+  // Default to dev.
+  let config = configDevelopment
+
+  // Production?
+  if (args.mode === 'production') {
+    config = configProduction
+  }
+
+  // Expose object.
+  return config
 }
