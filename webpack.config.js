43e75fa1509ea7c1642e705e558bc96048f6c873
nathansmith <nathan@sonspring.com>
2018-09-07 09:30:42 -0500

    For a "vendor" split, so our final component doens't have React bundled with it.

--- webpack.config.js
+++ webpack.config.js
@@ -15,87 +15,116 @@ const path = require('path')
 // ========
 
 const plugins = [
+  // CSS.
+  new MiniCssExtractPlugin({
+    filename: '[name].css'
+  }),
+
+  // HTML.
   new HtmlWebPackPlugin({
     template: './source/demo.html',
-    filename: './index.html'
-  }),
-  new MiniCssExtractPlugin({
-    filename: 'bundle.[hash].css'
+    filename: './index.html',
+    minify: {
+      collapseWhitespace: true,
+      collapseInlineTagWhitespace: true,
+      removeCDATASectionsFromCDATA: true,
+      removeComments: true,
+      removeCommentsFromCDATA: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      useShortDoctype: true
+    }
   })
 ]
 
-// ====================
-// Config: Development.
-// ====================
+// ======
+// Rules.
+// ======
 
-const configDevelopment = {
-  plugins,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ]
+const cssRuleForDev = {
+  test: /\.css$/,
+  use: [
+    'style-loader',
+    'css-loader',
+    'postcss-loader'
+  ]
+}
+
+const cssRuleForProd = {
+  test: /\.css$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    'postcss-loader'
+  ]
+}
+
+const jsRule = {
+  test: /\.js$/,
+  use: 'babel-loader'
+}
+
+// ==================
+// Split "vendor" JS.
+// ==================
+
+const optimization = {
+  splitChunks: {
+    cacheGroups: {
+      vendor: {
+        chunks: 'all',
+        enforce: true,
+        name: 'vendor',
+        test: /node_modules/
       }
-    ]
-  },
-
-  // Use source maps for dev.
-  devtool: 'source-map',
-
-  // Use demo as source.
-  entry: './source/demo.js',
-
-  /*
-    Flush cache here, by using
-    the hash in the file name.
-  */
-  output: {
-    filename: 'bundle.[hash].js'
+    }
   }
 }
 
-// ===================
-// Config: Production.
-// ===================
+// ====================
+// Entry: Source files.
+// ====================
 
-const configProduction = {
+const entry = {
+  /*
+    NOTE:
+
+    - Demo is not used for distribution.
+
+    - Index is not used for local demo.
+
+    It is easier from a config standpoint
+    to just keep them in the same `entry`.
+  */
+  demo: './source/demo.js',
+  index: './source/index.js'
+}
+
+// ====================
+// Output: Build files.
+// ====================
+
+const output = {
+  path: path.join(__dirname, 'build'),
+  filename: '[name].js'
+}
+
+// =======
+// Config.
+// =======
+
+const config = {
+  // Shared.
+  entry,
+  optimization,
+  output,
   plugins,
+
+  // Rules.
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader'
-        ]
-      }
+      jsRule
     ]
-  },
-
-  // Use component as source.
-  entry: './source/index.js',
-
-  /*
-    We do not use a hash in the file
-    name here, because it needs to be
-    consistent for NPM distribution.
-  */
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'index.js'
   }
 }
 
@@ -104,10 +133,16 @@ const configProduction = {
 // =======
 
 module.exports = (env, args = {}) => {
+  // Get rules.
+  const rules = config.module.rules
+
+  // Production?
+  if (args.mode === 'production') {
+    rules.push(cssRuleForProd)
+  } else {
+    rules.push(cssRuleForDev)
+  }
+
   // Expose object.
-  return (
-    args.mode === 'production'
-      ? configProduction
-      : configDevelopment
-  )
+  return config
 }
